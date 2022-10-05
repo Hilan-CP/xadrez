6fdd5edaf5ece99d18630f67c0f8da3bc389cd3f
Hilan-CP <hilan.paganini@gmail.com>
2022-10-04 21:04:16 -0300

    Criado pacote e classes da partida de xadrez

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,12 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		ChessMatch chess = new ChessMatch();
+		UI.printBoard(chess.getPieces());
 	}
 
 }
